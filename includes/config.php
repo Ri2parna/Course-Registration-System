0f1808a951cb6055e37f1da2c2b6b82c881655d3
Rituparna Das <ri2parna@outlook.com>
2020-07-03 02:03:57 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_USER','b5f4f6353381bc');
+define('DB_PASS' ,'9b84e03e');
 define('DB_NAME','TU_Student_Data');
 $database = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
